56b471d9887fbb8e6b43928fe0ef4badcda52373
zulfikar4568 <isnaen70@gmail.com>
2025-04-22 20:37:08 +0700

    feat: add test

---
+++ BuildDotnetApp.Test/MyMath.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using BuildDotnetApp;
+using FluentAssertions;
+
+namespace BuildDotnetApp.Test
+{
+    [TestClass]
+    public class MyMath
+    {
+        [TestMethod]
+        public void ItShouldBe5()
+        {
+            BuildDotnetApp.MyMath math = new BuildDotnetApp.MyMath();
+            var result = BuildDotnetApp.MyMath.Add(2, 3);
+            result.Should().Be(5);
+        }
+    }
+}
