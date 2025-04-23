eda5dce5c0c4b3fe25eca8bc932332cb6f74eda9
zulfikar4568 <isnaen70@gmail.com>
2025-04-24 00:20:46 +0700

    feat: update CLI

--- BuildDotnetApp.CLI/Program.cs
+++ BuildDotnetApp.CLI/Program.cs
@@ -10,7 +10,7 @@ namespace BuildDotnetApp.CLI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!!");
             Console.ReadLine();
         }
     }
