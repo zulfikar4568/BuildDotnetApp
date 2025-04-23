467fd4c2deab15fc9adf3c8dce0885a3061a58f4
zulfikar4568 <isnaen70@gmail.com>
2025-04-23 21:24:41 +0700

    fix: the wixobj

--- installer/build.ps1
+++ installer/build.ps1
@@ -33,10 +33,10 @@ Write-Host "Generating WiX fragment for main application..."
 heat dir $outputDirMain -gg -g1 -sf -sreg -srd -dr INSTALLFOLDER -cg ApplicationFiles -var var.SourceDir -out ".\installer\fragment.main.wxs"
 
 Write-Host "Compiling WiX files for main application..."
-candle -nologo -arch x64 -dSourceDir="$outputDirMain" -dVersion="$version" ".\installer\product.wxs" ".\installer\fragment.main.wxs" -out ".\installer\"
+candle -nologo -arch x64 -dSourceDir="$outputDirMain" -dVersion="$version" ".\installer\product.wxs" ".\installer\fragment.main.wxs" -out ".\installer\main\"
 
 Write-Host "Linking WiX files for main application..."
-light -nologo -ext WixUIExtension -ext WixUtilExtension -out ".\installer\v$version-x64.msi" ".\installer\product.wixobj" ".\installer\fragment.main.wixobj" -b "$outputDirMain"
+light -nologo -ext WixUIExtension -ext WixUtilExtension -out ".\installer\v$version-x64.msi" ".\installer\main\product.wixobj" ".\installer\main\fragment.main.wixobj" -b "$outputDirMain"
 
 # Build CLI application installer
 Write-Host "Building CLI application installer..."
@@ -44,13 +44,32 @@ Write-Host "Generating WiX fragment for CLI application..."
 heat dir $outputDirCli -gg -g1 -sf -sreg -srd -dr INSTALLFOLDER -cg ApplicationFiles -var var.SourceDir -out ".\installer\fragment.cli.wxs"
 
 Write-Host "Compiling WiX files for CLI application..."
-candle -nologo -arch x64 -dSourceDir="$outputDirCli" -dVersion="$version" ".\installer\product.cli.wxs" ".\installer\fragment.cli.wxs" -out ".\installer\"
+candle -nologo -arch x64 -dSourceDir="$outputDirCli" -dVersion="$version" ".\installer\product.cli.wxs" ".\installer\fragment.cli.wxs" -out ".\installer\cli\"
 
 Write-Host "Linking WiX files for CLI application..."
-light -nologo -ext WixUIExtension -ext WixUtilExtension -out ".\installer\v$version-cli-x64.msi" ".\installer\product.cli.wixobj" ".\installer\fragment.cli.wixobj" -b "$outputDirCli"
+light -nologo -ext WixUIExtension -ext WixUtilExtension -out ".\installer\v$version-cli-x64.msi" ".\installer\cli\product.cli.wixobj" ".\installer\cli\fragment.cli.wixobj" -b "$outputDirCli"
+
+# Sign the MSI files if certificate is available
+if ($env:SIGN_CERTIFICATE_PATH -and $env:SIGN_CERTIFICATE_PASSWORD) {
+    Write-Host "Signing MSI files..."
+    
+    # Import the certificate
+    $cert = Import-PfxCertificate -FilePath $env:SIGN_CERTIFICATE_PATH -Password (ConvertTo-SecureString -String $env:SIGN_CERTIFICATE_PASSWORD -AsPlainText -Force)
+    
+    # Sign the main application MSI
+    Write-Host "Signing main application MSI..."
+    signtool sign /tr http://timestamp.digicert.com /td sha256 /fd sha256 /a /f $env:SIGN_CERTIFICATE_PATH /p $env:SIGN_CERTIFICATE_PASSWORD ".\installer\v$version-x64.msi"
+    
+    # Sign the CLI application MSI
+    Write-Host "Signing CLI application MSI..."
+    signtool sign /tr http://timestamp.digicert.com /td sha256 /fd sha256 /a /f $env:SIGN_CERTIFICATE_PATH /p $env:SIGN_CERTIFICATE_PASSWORD ".\installer\v$version-cli-x64.msi"
+} else {
+    Write-Host "Warning: No code signing certificate provided. MSI files will not be signed."
+}
 
 # Clean up
 Write-Host "Cleaning up temporary files..."
 Remove-Item -Recurse -Force ".\installer\output"
-Remove-Item ".\installer\*.wixobj"
+Remove-Item -Recurse -Force ".\installer\main"
+Remove-Item -Recurse -Force ".\installer\cli"
 Remove-Item ".\installer\fragment.*.wxs" 
