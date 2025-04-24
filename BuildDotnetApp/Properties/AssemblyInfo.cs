b4fc58b427ef7a8c988da22d23ad7ef040ccf91a
zulfikar4568 <isnae70@gmail.com>
2025-04-24 14:29:49 +0000

    chore(update): v1.5.1 push to the AssemblyInfo.cs

--- BuildDotnetApp/Properties/AssemblyInfo.cs
+++ BuildDotnetApp/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.5.0.37")]
-[assembly: AssemblyFileVersion("1.5.0.37")]
+[assembly: AssemblyVersion("1.5.1.39")]
+[assembly: AssemblyFileVersion("1.5.1.39")]
